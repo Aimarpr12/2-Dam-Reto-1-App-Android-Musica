984ca34e53f720ac85cdc24596d618503af19a9f
Milena <milena.cuellarpa@elorrieta-errekamari.com>
2023-10-23 18:50:09 +0200

    song activity

---
+++ app/src/main/java/com/example/reto1/data/Song.kt
@@ -0,0 +1,11 @@
+package com.example.reto1.data
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Song(
+    val id: Int,
+    val url: String,
+    val title: String,
+    val author: String
+): Parcelable
