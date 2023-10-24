04a00caf317b56ec2cf5726293e4acacc07bd2f9
aimar <aimar.peleaar@elorrieta-elorrieta.com>
2023-10-24 16:39:06 +0200

    retrofit1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,20 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures {
+        compose = true
+        // ADD para que automaticamente asocie clases a vistas xml "MainActivityBinding" etc
+        viewBinding = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.3"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
 }
 
 dependencies {
