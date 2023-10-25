7b21817e017ce5b6f34e023fc9b04062200e2b6a
aimar <aimar.peleaar@elorrieta-elorrieta.com>
2023-10-25 20:04:33 +0200

    retrofit1

--- app/src/main/java/com/example/reto1/data/repository/remote/APIInterface.kt
+++ app/src/main/java/com/example/reto1/data/repository/remote/APIInterface.kt
@@ -5,6 +5,7 @@ import retrofit2.Response
 import retrofit2.http.DELETE
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.POST
 
 interface APIInterface {
